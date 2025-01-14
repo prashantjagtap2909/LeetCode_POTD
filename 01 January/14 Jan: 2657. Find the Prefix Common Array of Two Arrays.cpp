686d80d30f153fb8501ac85eb57b23f69a791e56
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2025-01-14 21:17:42 +0530

    Create 14 Jan: 2657. Find the Prefix Common Array of Two Arrays.cpp

---
+++ 01 January/14 Jan: 2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> freq(n + 1, 0);
+        vector<int> res;
+        int cnt = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (++freq[A[i]] == 2) cnt++;
+            if (++freq[B[i]] == 2) cnt++;
+            res.push_back(cnt);
+        }
+        return res;
+    }
+};
