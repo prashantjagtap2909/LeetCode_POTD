7e451e849d2306a109342115e84b7a2d85b4fd6e
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2025-04-10 22:06:18 +0530

    Update 10 Apr: 2999. Count the Number of Powerful Integers.cpp

--- 04 April/10 Apr: 2999. Count the Number of Powerful Integers.cpp
+++ 04 April/10 Apr: 2999. Count the Number of Powerful Integers.cpp
@@ -1,33 +1,41 @@
+long long tens[17];
+long long powB[17];
 class Solution {
 public:
-    long long numberOfPowerfulInt(long long start, long long finish, int limit, string suffix) {
-         auto countPowerfulUpTo = [&](int num) -> int {
-            string numStr = to_string(num);
-            int suffixLen = suffix.size();
-            int prefixLen = numStr.size() - suffixLen;
+    void compute_tens(){
+        if (tens[0]==1) return;
+        tens[0]=1;
+        for(int i=1; i<17; i++)
+            tens[i]=tens[i-1]*10LL;
+    }
+    void compute_powB(int B){
+        powB[0]=1;
+        for(int i=1; i<17; i++)
+            powB[i]=powB[i-1]*B;
+    }
 
-            if (prefixLen < 0) return 0;
-
-            vector<vector<int>> dp(prefixLen + 1, vector<int>(2, 0));
-
-            dp[prefixLen][0] = 1;
-            string suffixFromNum = numStr.substr(prefixLen);
-            dp[prefixLen][1] = stoi(suffixFromNum) >= stoi(suffix) ? 1 : 0;
-
-            for (int i = prefixLen - 1; i >= 0; --i) {
-                int digit = numStr[i] - '0';
-                dp[i][0] = (limit + 1) * dp[i + 1][0];
-
-                if (digit <= limit) {
-                    dp[i][1] = digit * dp[i + 1][0] + dp[i + 1][1];
-                } else {
-                    dp[i][1] = (limit + 1) * dp[i + 1][0];
-                }
+    long long cnt(long long x, long long sn, int ds, int limit){
+        if (x<sn) return 0;
+        long long x0=x, rem=x0%tens[ds];
+        x0/=tens[ds];
+        int d=lower_bound(tens, tens+17, x0)-tens;
+        long long cnt=0;
+        for( int i=d; i>=0; i--){//MSDF
+            auto [q, r]=ldiv(x0, tens[i]);
+            if (q>limit){
+                cnt+=powB[i+1];
+                return cnt;
             }
-
-            return dp[0][1];
-        };
-
-        return countPowerfulUpTo(finish) - countPowerfulUpTo(start - 1);
+            cnt+=powB[i]*q;
+            x0-=q*tens[i];
+        }
+        return cnt+(rem>=sn);
+    }
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string& s) {
+        compute_tens();
+        compute_powB(limit+1);
+        long long sn=stoll(s);
+        int ds=s.size(), d0=log10(start)+1, d1=log10(finish)+1;
+        return cnt(finish, sn, ds, limit)-cnt(start-1, sn, ds, limit);
     }
 };
