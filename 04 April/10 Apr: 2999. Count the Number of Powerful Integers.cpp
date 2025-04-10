d2005feab5c5683e0dba1f101bcb311bde4a03fa
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2025-04-10 22:04:44 +0530

    Create 10 Apr: 2999. Count the Number of Powerful Integers.cpp

---
+++ 04 April/10 Apr: 2999. Count the Number of Powerful Integers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+         auto countPowerfulUpTo = [&](int num) -> int {
+            string numStr = to_string(num);
+            int suffixLen = suffix.size();
+            int prefixLen = numStr.size() - suffixLen;
+
+            if (prefixLen < 0) return 0;
+
+            vector<vector<int>> dp(prefixLen + 1, vector<int>(2, 0));
+
+            dp[prefixLen][0] = 1;
+            string suffixFromNum = numStr.substr(prefixLen);
+            dp[prefixLen][1] = stoi(suffixFromNum) >= stoi(suffix) ? 1 : 0;
+
+            for (int i = prefixLen - 1; i >= 0; --i) {
+                int digit = numStr[i] - '0';
+                dp[i][0] = (limit + 1) * dp[i + 1][0];
+
+                if (digit <= limit) {
+                    dp[i][1] = digit * dp[i + 1][0] + dp[i + 1][1];
+                } else {
+                    dp[i][1] = (limit + 1) * dp[i + 1][0];
+                }
+            }
+
+            return dp[0][1];
+        };
+
+        return countPowerfulUpTo(finish) - countPowerfulUpTo(start - 1);
+    }
+};
