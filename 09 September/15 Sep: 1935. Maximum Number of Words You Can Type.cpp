13ff8cc2f805aa0d0753c3aa03a9d8cc258e95d3
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2025-09-15 23:00:00 +0530

    Create 15 Sep: 1935. Maximum Number of Words You Can Type.cpp

---
+++ 09 September/15 Sep: 1935. Maximum Number of Words You Can Type.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        vector<int> brokenKeyMap(26, 0);
+        int asciiA = 'a';
+
+        for (char c : brokenLetters) {
+            brokenKeyMap[c - asciiA] = 1;
+        }
+        int wordCount = 1;  
+        int brokenWordCount = 0;
+        bool wordHasBrokenKey = false;
+
+        for (char c : text) {
+            if (c == ' ') {
+                if (wordHasBrokenKey) brokenWordCount++;
+                wordHasBrokenKey = false;
+                wordCount++;
+            } else if (!wordHasBrokenKey && brokenKeyMap[c - asciiA] > 0) {
+                wordHasBrokenKey = true;
+            }
+        }
+
+        if (wordHasBrokenKey) brokenWordCount++;
+
+        return wordCount - brokenWordCount;
+    }
+};
